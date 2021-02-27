83fd665974d00aa14bb301e6f31bf9d8b8e16a8b
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-02-27 00:11:14 -0500

    waiting for Gavin - possible wrong build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
+
 export class App extends Component {
   Render() {
-    return <main></main>
+    return (
+      <main>
+        <h1> Color Picker</h1>
+
+        <div>
+          {/* create labels for each color slide */}
+          <label>
+            <h2>
+              Hue
+              <input name="hue" type="range" min="0" max="360" />
+            </h2>
+          </label>
+          <label>
+            <h2>
+              Saturation
+              <input name="saturation" type="range" min="0" max="100" />
+            </h2>
+          </label>
+          <label>
+            <h2>
+              Lightness
+              <input name="lightness" type="range" min="0" max="100" />
+            </h2>
+          </label>
+        </div>
+      </main>
+    )
   }
 }
