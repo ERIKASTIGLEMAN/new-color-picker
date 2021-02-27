fd3644cc76b49e7003c1dc25d6b416627d1550ff
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-02-26 23:49:20 -0500

    Setting up Intial Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-
-export function App() {
-  return <div>Hello, World!</div>
+import React, { Component } from 'react'
+export class App extends Component {
+  Render() {
+    return <main></main>
+  }
 }
